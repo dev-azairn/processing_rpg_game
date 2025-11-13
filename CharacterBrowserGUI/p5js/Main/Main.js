8bb8469c904376d7942ede1ee4faca36bf25af04
ppanturat <punnaporn2548@gmail.com>
2025-11-13 21:52:46 +0700

    create Sprite.js
    I use folder 'data' to stored images, if you use different name, make sure to change it. Also added preload(), idk if i need to put in main or unit, feel free to change na.

--- CharacterBrowserGUI/p5js/Main/Main.js
+++ CharacterBrowserGUI/p5js/Main/Main.js
@@ -1,7 +1,13 @@
-function setup() {
+let configData;
 
+function preload() {
+  // Preload all config files - this runs before setup()
+  configData = loadStrings('YourConfigFile.txt');
 }
 
+function setup() {
+  spriteName = new Sprite(configData);
+}
 
 function draw() {
 
